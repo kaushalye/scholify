9f9c5777ee16cee7d763985e517afc75e9b7d40a
kaushalye <kaushalye@gmail.com>
2016-05-01 00:49:01 +1000

    selection boxes for students and sponsors when creating scholarships

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,4 +1,8 @@
 class Sponsor < ActiveRecord::Base
   has_many :scholarships
   has_many :donations
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,9 @@
 class Student < ActiveRecord::Base
   has_many :scholarships
   has_many :payments
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
