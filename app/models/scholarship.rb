9d36519766dd00d769658d494b4be758b45967ab
kaushalye <kaushalye@gmail.com>
2016-05-01 12:07:31 +1000

    ui change

--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -3,4 +3,6 @@ class Scholarship < ActiveRecord::Base
   belongs_to :sponsor
   has_many :donations
   has_many :payments
+
+  STATUSES = ['ACTIVE', 'INACTIVE', 'NOT STARTED']
 end
--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,7 +1,7 @@
 class Sponsor < ActiveRecord::Base
   has_many :scholarships
   has_many :donations
-
+  TITLES =  ['Mr', 'Mrs', 'Ms', 'Dr']
   def full_name
     "#{first_name} #{last_name}"
   end
