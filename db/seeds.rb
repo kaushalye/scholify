dcb30360a4ade7b0ccd0fb9b30e1cecdd034347f
kaushalye <kaushalye@gmail.com>
2016-05-01 17:35:00 +1000

    seeding for payment

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,21 @@ Scholarship.create(amount:25,
                    student_id:1,
                    created_at:Time.now.to_datetime,
                    updated_at:Time.now.to_datetime)
+
+Donation.create(sponsor_id:1,
+                 scholarship_id:1,
+                 amount:250,
+                 valid_from:DateTime.strptime("09/01/2016 19:00", "%m/%d/%Y %H:%M"),
+                 num_of_months: 12,
+                 created_at:Time.now.to_datetime,
+                 updated_at:Time.now.to_datetime)
+
+Payment.create(student_id:1,
+                scholarship_id:1,
+                amount:2500,
+                from_account:12345,
+                to_account:45678,
+                external_ref: "ref",
+                notes: "Some notes",
+                created_at:Time.now.to_datetime,
+                updated_at:Time.now.to_datetime)
