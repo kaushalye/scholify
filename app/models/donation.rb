a2db509672f0269ebff73f95c0b4acfb891230fc
kaushalye <kaushalye@gmail.com>
2016-05-02 22:36:46 +1000

    validation for scholarship and donation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,7 @@
 class Donation < ActiveRecord::Base
   belongs_to :scholarship
   belongs_to :sponsor
+
+  validates :scholarship, presence: true
+  validates :sponsor, presence: true
 end
--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -4,5 +4,8 @@ class Scholarship < ActiveRecord::Base
   has_many :donations
   has_many :payments
 
+  validates :student, presence: true
+  validates :sponsor, presence: true
+
   STATUSES = ['ACTIVE', 'INACTIVE', 'NOT STARTED']
 end
