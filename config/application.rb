0cde29983225cf6b3b784840532a1864d88d3b27
kaushalye <kaushalye@gmail.com>
2016-05-16 20:30:39 +1000

    avoid precompiling

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Scholify
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
