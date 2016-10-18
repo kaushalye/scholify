5d067d4099b02f09c1beb77b31c21f3808e2e283
Indika Meedeniya <indikamee@gmail.com>
2016-10-18 21:48:07 +1100

    payments bug fix

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -17,25 +17,25 @@ class Payment < ActiveRecord::Base
   def self.eligible_payments
     payments = []
     schols = Scholarship.eligible_all
-    schols.each do |schol|
-      payment_index = "#{schol.start_date.year}#{schol.start_date.month}".to_i - 1
-      last_payment = Payment.last_payment(schol.id)
-      #This schol has a payment entry, start from next month
-      #Otherwise use the start month of the scholarship
-      #if ( last_payment )
-      #  payment_index = last_payment.effective_month.to_i + 1
-      #end
+    #schols.each do |schol|
+    #  payment_index = "#{schol.start_date.year}#{schol.start_date.month}".to_i - 1
+    #  last_payment = Payment.last_payment(schol.id)
+    #  #This schol has a payment entry, start from next month
+    #  #Otherwise use the start month of the scholarship
+    #  #if ( last_payment )
+    #  #  payment_index = last_payment.effective_month.to_i + 1
+    #  #end
 
-      payments_up_to = Date.today + 1.month
-      payment_index_up_to = "#{payments_up_to.year}#{payments_up_to.month}".to_i - 1
-      #insert the eliegible payment
-      (payment_index .. payment_index_up_to ).each do |i|
-        payment = Payment.new
-        payment.scholarship = schol
-        payment.effective_month = i
-        payments.push payment
-      end
-    end
+    #  payments_up_to = Date.today + 1.month
+    #  payment_index_up_to = "#{payments_up_to.year}#{payments_up_to.month}".to_i - 1
+    #  #insert the eliegible payment
+    #  (payment_index .. payment_index_up_to ).each do |i|
+    #    payment = Payment.new
+    #    payment.scholarship = schol
+    #    payment.effective_month = i
+    #    payments.push payment
+    #  end
+    #end
 
     payments
   end
