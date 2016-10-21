c6d22f15033f3b08e80f2066058c38fc95050982
Indika Meedeniya <indikamee@gmail.com>
2016-10-22 06:48:57 +1100

    ignore env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,8 @@
 export SECRET_KEY_BASE=GENERATED_CODE
-export GOOGLE_CLIENT_ID=826847929665-jfvoojiej98mhkftsm6jb371lbkur2hr.apps.googleusercontent.com
-export GOOGLE_SECRET=92r0Igeqsa1nrBVjz1GvD-jA
+#export GOOGLE_CLIENT_ID=826847929665-jfvoojiej98mhkftsm6jb371lbkur2hr.apps.googleusercontent.com
+#export GOOGLE_SECRET=92r0Igeqsa1nrBVjz1GvD-jA
 
+
+# UMAAV.org.au
+export GOOGLE_CLIENT_ID=281325989480-nkqbcs2aai9a6hclohc90culhn6ibss2.apps.googleusercontent.com 
+export GOOGLE_SECRET=hMNGB-tM_Q6QqiDDiJ15zyHZ 
